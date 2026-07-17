5f9a9d69b6424e34f8600dbd80ace3a07e3740fc
Thexrokmc <mastspths@gmail.com>
2026-07-17 21:29:09 +0300

    Update portfolio_engine.py

--- src/portfolio/portfolio_engine.py
+++ src/portfolio/portfolio_engine.py
@@ -1,9 +1,28 @@
+import json
+
+from portfolio.portfolio import Portfolio
+from portfolio.asset import Asset
+
+
 class PortfolioEngine:
-    def __init__(self):
-        self.assets = []
 
     def load_portfolio(self):
-        print("📂 Portfolio loaded.")
 
-    def summary(self):
-        print(f"Assets in portfolio: {len(self.assets)}")
+        portfolio = Portfolio()
+
+        with open("config/portfolio.json", "r") as file:
+
+            data = json.load(file)
+
+        for item in data.get("assets", []):
+
+            asset = Asset(
+                symbol=item["symbol"],
+                quantity=item["quantity"],
+                average_cost=item["average_cost"],
+                current_price=item.get("current_price", 0)
+            )
+
+            portfolio.add_asset(asset)
+
+        return portfolio
