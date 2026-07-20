12396b0f591cb73343b3f70c08dcec73abca1d7e
Thexrokmc <mastspths@gmail.com>
2026-07-21 02:24:54 +0300

    Update fact_generator.py

--- src/data/fact_generator.py
+++ src/data/fact_generator.py
@@ -1,6 +1,10 @@
 from data.market import MarketData
 from data.fact import Fact
-from data.fact_ids import PRICE_ABOVE_EMA200, PRICE_ABOVE_EMA50
+from data.fact_ids import (
+    PRICE_ABOVE_EMA200,
+    PRICE_ABOVE_EMA50,
+    PRICE_BELOW_EMA200,
+)
 
 
 def add_fact(facts: list[Fact], fact_id: str, value: bool):
@@ -27,6 +31,13 @@ def market_data_to_facts(market: MarketData) -> list[Fact]:
             facts,
             PRICE_ABOVE_EMA50,
             market.price > market.ema50
+        )
+
+    if market.price is not None and market.ema200 is not None:
+        add_fact(
+            facts,
+            PRICE_BELOW_EMA200,
+            market.price < market.ema200
         )
 
     return facts
