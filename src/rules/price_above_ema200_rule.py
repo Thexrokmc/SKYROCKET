10f797aa14f14f105c50b808da86ab4353ac8012
Thexrokmc <mastspths@gmail.com>
2026-07-21 02:31:59 +0300

    Update price_above_ema200_rule.py

--- src/rules/price_above_ema200_rule.py
+++ src/rules/price_above_ema200_rule.py
@@ -1,8 +1,24 @@
 from rules.base_rule import BaseRule
+from rules.rule_result import RuleResult
 from data.fact_ids import PRICE_ABOVE_EMA200
 
 
 class PriceAboveEMA200Rule(BaseRule):
 
-    def evaluate(self, facts) -> bool:
-        return facts.get(PRICE_ABOVE_EMA200, False)
+    def __init__(self):
+        super().__init__(
+            name="Price Above EMA200",
+            weight=20
+        )
+
+    def evaluate(self, facts: dict) -> RuleResult:
+        passed = facts.get(
+            PRICE_ABOVE_EMA200,
+            False
+        )
+
+        return RuleResult(
+            name=self.name,
+            passed=passed,
+            weight=self.weight
+        )
