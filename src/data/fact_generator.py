4863a03b4037c740ccbda1a9e10920f6ea852633
Thexrokmc <mastspths@gmail.com>
2026-07-21 02:19:46 +0300

    Update fact_generator.py

--- src/data/fact_generator.py
+++ src/data/fact_generator.py
@@ -2,6 +2,13 @@ from data.market import MarketData
 from data.fact import Fact
 from data.fact_ids import PRICE_ABOVE_EMA200, PRICE_ABOVE_EMA50
 
+def add_fact(facts: list[Fact], fact_id: str, value: bool):
+    facts.append(
+        Fact(
+            id=fact_id,
+            value=value
+        )
+    )
 def market_data_to_facts(market: MarketData) -> list[Fact]:
     facts = []
 
