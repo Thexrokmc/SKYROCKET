d82ae5f813e4c1bae7d56499c333551c0bcf07c8
Thexrokmc <mastspths@gmail.com>
2026-07-21 09:08:54 +0300

    Update portfolio.py

--- src/portfolio/portfolio.py
+++ src/portfolio/portfolio.py
@@ -1,21 +1,18 @@
-from .asset import Asset
-
-
 class Portfolio:
+
     def __init__(self):
+
         self.assets = []
 
-    def add_asset(self, asset: Asset):
+    def add(self, asset):
+
         self.assets.append(asset)
 
-    @property
     def total_value(self):
-        return sum(asset.value for asset in self.assets)
 
-    @property
-    def total_cost(self):
-        return sum(asset.cost for asset in self.assets)
+        total = 0
 
-    @property
-    def total_pnl(self):
-        return self.total_value - self.total_cost
+        for asset in self.assets:
+            total += asset.market_value
+
+        return total
