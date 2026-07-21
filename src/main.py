699294dae926358c5472c764004bf2bb87212e76
Thexrokmc <mastspths@gmail.com>
2026-07-21 03:20:37 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from data.fact_generator import market_data_to_facts
 from rules.price_above_ema200_rule import PriceAboveEMA200Rule
 from rules.price_above_ema50_rule import PriceAboveEMA50Rule
 from rules.rsi_oversold_rule import RSIOversoldRule
+from rules.macd_bullish_rule import MACDBullishRule
+from rules.macd_bearish_rule import MACDBearishRule
 from rules.allocation_rule import AllocationRule
 
 from engines.rule_engine import RuleEngine
@@ -34,6 +36,14 @@ def main():
 
     rule_engine.add_rule(
         RSIOversoldRule()
+    )
+
+    rule_engine.add_rule(
+        MACDBullishRule()
+    )
+
+    rule_engine.add_rule(
+        MACDBearishRule()
     )
 
     rule_engine.add_rule(
