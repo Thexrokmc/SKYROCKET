73321111c0e04e522ad2b5aca16b9cc415507436
Thexrokmc <mastspths@gmail.com>
2026-07-21 09:18:07 +0300

    Refactor PriceAboveEMA200Rule initialization and evaluation

--- src/rules/price_above_ema200_rule.py
+++ src/rules/price_above_ema200_rule.py
@@ -1,17 +1,22 @@
 from rules.base_rule import BaseRule
 from rules.rule_result import RuleResult
-from data.fact_ids import PRICE_ABOVE_EMA200
+
+from data.fact_ids import (
+    PRICE_ABOVE_EMA200
+)
 
 
 class PriceAboveEMA200Rule(BaseRule):
 
     def __init__(self):
+
         super().__init__(
             name="Price Above EMA200",
-            weight=20
+            weight=15
         )
 
     def evaluate(self, facts: dict) -> RuleResult:
+
         passed = facts.get(
             PRICE_ABOVE_EMA200,
             False
@@ -20,5 +25,6 @@ class PriceAboveEMA200Rule(BaseRule):
         return RuleResult(
             name=self.name,
             passed=passed,
-            weight=self.weight
+            weight=self.weight,
+            description="Price is above EMA200"
         )
