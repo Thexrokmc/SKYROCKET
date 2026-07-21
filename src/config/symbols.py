86cd7b107571e325283b0608c807f908c7d45f4f
Thexrokmc <mastspths@gmail.com>
2026-07-21 09:41:29 +0300

    Add Symbols class with cryptocurrency constants

---
+++ src/config/symbols.py
@@ -0,0 +1,76 @@
+class Symbols:
+
+    BTC = "PF_XBTUSD"
+
+    ETH = "PF_ETHUSD"
+
+    XRP = "PF_XRPUSD"
+
+    SOL = "PF_SOLUSD"
+
+    LINK = "PF_LINKUSD"
+
+    SUI = "PF_SUIUSD"
+
+    ADA = "PF_ADAUSD"
+
+    AVAX = "PF_AVAXUSD"
+
+    DOT = "PF_DOTUSD"
+
+    ATOM = "PF_ATOMUSD"
+
+    NEAR = "PF_NEARUSD"
+
+    RENDER = "PF_RENDERUSD"
+
+    ARB = "PF_ARBUSD"
+
+    IMX = "PF_IMXUSD"
+
+    FET = "PF_FETUSD"
+
+    AAVE = "PF_AAVEUSD"
+
+    ZEC = "PF_ZECUSD"
+
+    @classmethod
+    def all(cls):
+
+        return [
+
+            cls.BTC,
+
+            cls.ETH,
+
+            cls.XRP,
+
+            cls.SOL,
+
+            cls.LINK,
+
+            cls.SUI,
+
+            cls.ADA,
+
+            cls.AVAX,
+
+            cls.DOT,
+
+            cls.ATOM,
+
+            cls.NEAR,
+
+            cls.RENDER,
+
+            cls.ARB,
+
+            cls.IMX,
+
+            cls.FET,
+
+            cls.AAVE,
+
+            cls.ZEC
+
+        ]
