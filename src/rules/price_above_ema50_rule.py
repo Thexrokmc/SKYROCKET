04910e26f532ff5ed5f71dff00ada9b442f489f0
Thexrokmc <mastspths@gmail.com>
2026-07-21 03:08:31 +0300

    Create price_above_ema50_rule.py

---
+++ src/rules/price_above_ema50_rule.py
@@ -0,0 +1,24 @@
+from rules.base_rule import BaseRule
+from rules.rule_result import RuleResult
+from data.fact_ids import PRICE_ABOVE_EMA50
+
+
+class PriceAboveEMA50Rule(BaseRule):
+
+    def __init__(self):
+        super().__init__(
+            name="Price Above EMA50",
+            weight=10
+        )
+
+    def evaluate(self, facts: dict) -> RuleResult:
+        passed = facts.get(
+            PRICE_ABOVE_EMA50,
+            False
+        )
+
+        return RuleResult(
+            name=self.name,
+            passed=passed,
+            weight=self.weight
+        )
