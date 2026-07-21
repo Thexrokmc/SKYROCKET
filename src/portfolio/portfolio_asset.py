e3b40f601193d7794d4ea3a2e8ce9d147716c5fb
Thexrokmc <mastspths@gmail.com>
2026-07-21 09:07:30 +0300

    Create PortfolioAsset class with initial attributes

---
+++ src/portfolio/portfolio_asset.py
@@ -0,0 +1,19 @@
+class PortfolioAsset:
+
+    def __init__(self):
+
+        self.symbol = ""
+
+        self.amount = 0.0
+
+        self.average_price = 0.0
+
+        self.current_price = 0.0
+
+        self.market_value = 0.0
+
+        self.pnl = 0.0
+
+        self.score = 0.0
+
+        self.action = ""
